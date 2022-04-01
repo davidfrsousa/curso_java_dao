db2e7bd4b3f3e557125519e9c9df18df48b9835c
tndg1 <davidfrsousa@hotmail.com>
2022-04-01 16:58:37 -0300

    Implemented factory of DAO objects

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,9 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDAO;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,9 +12,13 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Seller seller = new Seller(1, "David","david@gmail.com ", new Date(), 3000.0, new Department(1, "Books"));
+		Seller seller = new Seller(1, "David", "david@gmail.com ", new Date(), 3000.0, new Department(1, "Books"));
 		System.out.println(seller);
-		
+
+		SellerDAO sellerDao = DaoFactory.createSellerDao();
+		DepartmentDAO departmentDao = DaoFactory.createDepartmentDao();
+		System.out.println(sellerDao);
+		System.out.println(departmentDao);
 	}
 
 }
